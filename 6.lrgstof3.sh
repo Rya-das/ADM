bbb4d4d1b05bd43377d6138461d37fdfb798827d
Rya_das <aiswaryakdas999@gmail.com>
2022-04-25 20:33:17 +0530

    Largest of Three Numbers

---
+++ 6.lrgstof3.sh
@@ -0,0 +1,16 @@
+echo "Enter 3 numbers"
+read a b c
+if[ $a -gt $b ]
+then
+if[ $a -gt $c]
+the
+echo "$a is largest"
+fi
+elif[ $b -gt $c ]
+then
+if[ $b -gt $a ]
+then echo "Largest is $b"
+fi
+else
+echo "Largest is $c"
+fi
